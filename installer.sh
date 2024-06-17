b5bcd665af1229b07f9a6bd2b0b994806e7cd270
Albaro Pereyra <AlbaroPereyra22@gmail.com>
2024-06-17 16:56:00 -0700

    updated installer.sh

--- installer.sh
+++ installer.sh
@@ -14,8 +14,8 @@ fileName=${repoUrl##*/};
 personalZshrcFile="${HOME}/.personalZshrc";
 uname=$(uname -r)
 WSL2=${uname##*-}
-WSL2=${WSL2%% *}
 
+git clone $repoUrl ${HOME}/${fileName};
 if [ "X$(uname -s)" = "XDarwin" ];
 then
   # -s returns 0 if found otherwise 1
@@ -33,20 +33,20 @@ then
     # Install zsh
     brew install zsh;
   fi
-  git clone $repoUrl ${HOME}/${fileName};
   mv ${HOME}/${fileName}/.zshrc ${HOME}/.zshrc;
-elif[ "X$WSL2" = "XWSL2" ];
-  echo "WSL2"
+elif [ "X$WSL2" = "XWSL2" ];
+then
+  sudo apt-get updates;
+  sudo apt-get upgrade;
+  sudo apt install bat;
+  mv ${HOME}/${fileName}/.zshrc$WSL2 ${HOME}/.zshrc;
+  touch $personalZshrcFile
 fi
-echo "$WSL2"
+rm -rf ${HOME}/${fileName};
 if [ ! -e ${HOME}/.oh-my-zsh ];
 then
   # Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 fi
 
-# git clone $repoUrl ${HOME}/${fileName};
-# mv ${HOME}/${fileName}/.zshrc ${HOME}/.zshrc;
-# touch $personalZshrcFile
-# rm -rf ${HOME}/${fileName};
 printf "ZSH conf has been updated.\n";
